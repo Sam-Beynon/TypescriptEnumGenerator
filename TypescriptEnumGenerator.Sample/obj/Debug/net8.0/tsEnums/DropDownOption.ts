22ece0061cfbe39cbeef3db579033927f0b1b852
Sam Beynon <sam.j.beynon@outlook.com>
2024-06-08 00:40:02 +0800

    Trying to fix gitIgnore sync issues

--- TypescriptEnumGenerator.Sample/obj/Debug/net8.0/tsEnums/DropDownOption.ts
+++
@@ -1,9 +0,0 @@
-export default class DropdownOption {
-    id: number;
-    label: string;
-
-    constructor(id: number, label: string) {
-        this.id = id;
-        this.label = label;
-    }
-}
